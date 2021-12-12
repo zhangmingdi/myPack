ecb64a7e234fb2458c828608a9e89ec454455a8f
zhangmingdi <3201896772@qq.com>
2021-12-12 11:08:25 +0800

    简易的webpack钩子

--- bin/index.js
+++ bin/index.js
@@ -9,5 +9,5 @@ const config = require(path.resolve('webpack.config.js'));
 const Compiler = require('../lib/Compiler.js')
 
 const compiler = new Compiler(config)
-
+compiler.hooks.entryOptions.call()
 compiler.run()
